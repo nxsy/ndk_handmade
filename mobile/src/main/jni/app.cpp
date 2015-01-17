eea652b6cab41724a222b87a041dc20c9bb11713
Neil Blakey-Milner <nbm@nxsy.org>
2015-01-16 18:03:55 -0800

    Writing to the window using bitmap

--- mobile/src/main/jni/app.cpp
+++ mobile/src/main/jni/app.cpp
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -68,6 +69,28 @@ int32_t on_input_event(android_app *app, AInputEvent *event) {
     return 0;
 }
 
+void draw(android_app *app)
+{
+    user_data *p = (user_data *)app->userData;
+    static uint8_t grey_value = 0;
+    grey_value += 13;
+    ANativeWindow_Buffer lWindowBuffer;
+    ANativeWindow* lWindow = app->window;
+    if (!app->window)
+    {
+        return;
+    }
+    ANativeWindow_setBuffersGeometry(lWindow, 0, 0, WINDOW_FORMAT_RGBA_8888);
+
+    if (ANativeWindow_lock(lWindow, &lWindowBuffer, NULL) < 0) {
+        return;
+    }
+
+    memset(lWindowBuffer.bits, grey_value, lWindowBuffer.stride*lWindowBuffer.height*sizeof(uint32_t));
+
+    ANativeWindow_unlockAndPost(lWindow);
+}
+
 void android_main(android_app *app) {
     app_dummy();
 
@@ -77,45 +100,74 @@ void android_main(android_app *app) {
 
     app->onAppCmd = on_app_cmd;
     app->onInputEvent = on_input_event;
+    uint64_t counter;
+    while (++counter) {
+        timespec start_time = {};
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
 
-    while (1) {
         int poll_result, events;
         android_poll_source *source;
-        poll_result = ALooper_pollAll(-1, 0, &events, (void**)&source);
-        if (poll_result >= 0)
+
+        while((poll_result = ALooper_pollAll(0, 0, &events, (void**)&source)) >= 0)
         {
             source->process(app, source);
         }
-        else
+
+        switch (poll_result)
         {
-            switch (poll_result)
+            case ALOOPER_POLL_WAKE:
             {
-                case ALOOPER_POLL_WAKE:
-                {
-                    __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_WAKE");
-                    break;
-                }
-                case ALOOPER_POLL_CALLBACK:
-                {
-                    __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_CALLBACK");
-                    break;
-                }
-                case ALOOPER_POLL_TIMEOUT:
-                {
-                    __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_TIMEOUT");
-                    break;
-                }
-                case ALOOPER_POLL_ERROR:
-                {
-                    __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_ERROR");
-                    break;
-                }
-                default:
-                {
-                    __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was %d", poll_result);
-                    break;
-                }
+                __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_WAKE");
+                break;
+            }
+            case ALOOPER_POLL_CALLBACK:
+            {
+                __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_CALLBACK");
+                break;
+            }
+            case ALOOPER_POLL_TIMEOUT:
+            {
+                //__android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_TIMEOUT");
+                break;
+            }
+            case ALOOPER_POLL_ERROR:
+            {
+                __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was ALOOPER_POLL_ERROR");
+                break;
+            }
+            default:
+            {
+                __android_log_print(ANDROID_LOG_INFO, p.app_name, "poll_result was %d", poll_result);
+                break;
             }
         }
+
+        draw(app);
+
+        timespec end_time = {};
+        clock_gettime(CLOCK_MONOTONIC_RAW, &end_time);
+
+        end_time.tv_sec -= start_time.tv_sec;
+        start_time.tv_sec -= start_time.tv_sec;
+
+        int64_t time_taken = ((end_time.tv_sec * 1000000000 + end_time.tv_nsec) -
+            (start_time.tv_sec * 1000000000 + start_time.tv_nsec));
+
+        int64_t time_to_sleep = 50 * 1000000;
+        if (time_taken <= time_to_sleep)
+        {
+            timespec sleep_time = {};
+            sleep_time.tv_nsec = time_to_sleep - time_taken;
+            timespec remainder = {};
+            nanosleep(&sleep_time, &remainder);
+        }
+        else
+        {
+            if (counter % 10 == 0)
+            {
+                __android_log_print(ANDROID_LOG_INFO, p.app_name, "Skipped frame!  Took %" PRId64 " ns total", time_taken);
+            }
+        }
+
     }
 }
